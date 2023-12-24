eeeb9bf8361c64464df8206085009a5e469187b6
Lucas Costa <francisco.lucas0507@gmail.com>
2023-12-24 09:58:01 -0300

    last fm search endpoint

---
+++ app/api/default_responses.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+
+export const ServerErrorResponse = (error: any = null) =>
+  new NextResponse(error !== null ? JSON.stringify(error) : null, {
+    status: 500,
+  })
+
+
+export const InvalidDataResponse = () => {
+    return new NextResponse(null, {
+      status: 400,
+    })
+}
--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,70 +1,31 @@
 import { NextResponse } from 'next/server'
-import { AUTH_URL, AUTH_HEADER, SPOTIFY_SEARCH } from './spotifyHelpers'
-import { QUERY_PARSER } from './spotifyParser'
-
-import type { SPOTIFY_TOKEN } from './spotifyHelpers'
-
-let currentToken: SPOTIFY_TOKEN | null = null
-let lastAuthTime: number
-
-const Authenticate = async () => {
-  const response = await fetch(AUTH_URL, {
-    method: 'POST',
-    headers: {
-      Authorization: AUTH_HEADER,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      grant_type: 'client_credentials',
-    }),
-  })
-
-  if (!response.ok) {
-    throw `Invalid Spotify Credentials \n ${response.status} ${response.statusText}}`
-  }
-
-  const token = (await response.json()) as SPOTIFY_TOKEN
-
-  lastAuthTime = new Date().getTime()
-  currentToken = token
-}
-
-const getToken = () => {
-  return `${currentToken!.token_type} ${currentToken!.access_token}`
-}
+import { ServerErrorResponse , InvalidDataResponse} from '../default_responses'
+import { LASTFM_ARTIST_SEARCH, LASTFM_BASE } from './lastfmHelpers'
+import { LASTFM_ARTIST_RESPONSE_PARSER } from './lastfmParsers'
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
+
   const query = searchParams.get('q')
 
-  if (query === null || query === undefined || query === '') {
-    return new NextResponse(null, {
-      status: 400,
-    })
+  if (typeof query !== 'string') {
+    return InvalidDataResponse()
   }
 
-  const timeSinceLastAuth = Math.floor(
-    (new Date().getTime() - lastAuthTime) / 1000
-  )
+  const url = LASTFM_ARTIST_SEARCH(query)
+  const res = await fetch(url)
 
-  if (currentToken === null || timeSinceLastAuth > currentToken?.expires_in) {
-    await Authenticate()
+  if(!res.ok){
+    return ServerErrorResponse()
   }
 
-  const res = await fetch(`${SPOTIFY_SEARCH}?q=${query}&type=artist`, {
-    headers: {
-      Authorization: getToken(),
-    },
-  })
+  const _data = await res.json()
 
-  const data = await res.json()
-  const search = QUERY_PARSER.safeParse(data)
+  const data = LASTFM_ARTIST_RESPONSE_PARSER.safeParse(_data)
 
-  if (!search.success) {
-    return new NextResponse(null, {
-      status: 400,
-    })
+  if (!data.success) {
+    return ServerErrorResponse()
   }
 
-  return NextResponse.json(search.data)
+  return NextResponse.json(data)
 }
