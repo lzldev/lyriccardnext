e23071a827ed5f3f1d8da27f90d4c64fbd262bee
Lucas Costa <francisco.lucas0507@gmail.com>
2024-01-03 09:09:32 -0300

    fix lyric card flex wrap

--- app/components/LyricCardGenerator.tsx
+++ app/components/LyricCardGenerator.tsx
@@ -6,8 +6,8 @@ import { LyricCard } from './LyricCard'
 import { LyricCardOptions } from './LyricCardOptions'
 export const LyricCardGenerator = () => {
   return (
-    <div className='shadow-woodsmoke-500 flex h-full min-h-full w-full flex-grow flex-col items-center justify-stretch'>
-      <div className='flex h-full w-full flex-1 flex-col gap-y-4 p-4 md:max-w-6xl md:p-0 md:pt-6'>
+    <div className='flex h-full min-h-full w-full flex-grow flex-col items-center justify-stretch shadow-woodsmoke-500'>
+      <div className='flex h-full w-full flex-1 flex-col gap-y-4 p-4 md:max-w-6xl md:pt-6'>
         <ArtistsQuery />
         <ImagePicker />
         <div className='flex w-full flex-row-reverse flex-wrap gap-4 md:flex-row'>
